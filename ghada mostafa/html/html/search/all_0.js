213a8c56977f24f1645f3e0b7d3829ca33822b92
ghada mostafa <mg133824@gmail.com>
2022-03-23 18:20:32 -0700

    add doxygen doc

---
+++ ghada mostafa/html/html/search/all_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['lcd_2eh_0',['LCD.h',['../_l_c_d_8h.html',1,'']]],
+  ['lcd_5fgotoxy_1',['LCD_GOTOXY',['../_l_c_d_8h.html#a514bd40849976f47af8d3ba30f6a4072',1,'LCD.h']]],
+  ['lcd_5finit_2',['LCD_INIT',['../_l_c_d_8h.html#af45b03cc2189d13d6f60c54864e0e0e8',1,'LCD.h']]],
+  ['lcd_5fvidsetcommand_3',['LCD_VIDSETCOMMAND',['../_l_c_d_8h.html#a8f1cc3691c599fbbc9c94509fa93f4f7',1,'LCD.h']]],
+  ['lcd_5fvidwritechar_4',['LCD_VIDWRITECHAR',['../_l_c_d_8h.html#a3de24e642158d00f1195b369c9c58aea',1,'LCD.h']]],
+  ['lcd_5fvidwritestrin_5',['LCD_VIDWRITESTRIN',['../_l_c_d_8h.html#afa36d84ea0548f030d700bf9f2f86de9',1,'LCD.h']]]
+];
